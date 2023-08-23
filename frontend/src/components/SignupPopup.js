d0a5d5a1d32c3b96215bb59444be5bc2e22501b4
Jynel Kealon <jkealon13@r2happrentice.org>
2023-08-22 21:08:32 -0400

    final changes 8/22

---
+++ frontend/src/components/SignupPopup.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+const SignupPopup = ({ isOpen, onClose }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState();
+
+  const handleEmailChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    ///
+  }
+  if (!isOpen) return null;
+
+  return (
+    <div className="signup-popup">
+      <div className="popup-content">
+      <h2>Login or Sign Up</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+          <button type="submit">Submit</button>
+        </form>
+        
+      </div>
+    </div>
+  );
+};
+
+export default SignupPopup;
