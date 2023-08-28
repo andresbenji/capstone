00a6e991bfd4beb0e489dcab5ff6a19f1bc9ffc9
Johnsondoe119 <johnsondoe119@gmail.com>
2023-08-27 21:43:52 -0400

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,7 @@ import RentalsPage from "./components/rentalPage";
 // import Slideshow from './components/slideshow';
 // import Testimonial from './components/testimonials';
 // import LanguageSwitcher from './components/LanguageSwitcher';
-<<<<<<< HEAD
-import './App.css';
-import './components/about.css'
-import './components/footer.css';
-import './components/home.css';
-import './components/nav.css';
-import './components/slideshow.css';
-import './components/testimonials.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import TestZillowApi from './components/testZillowApi';
 
-=======
 import "./App.css";
 import "./components/about.css";
 import "./components/footer.css";
@@ -25,11 +14,8 @@ import "./components/nav.css";
 import "./components/slideshow.css";
 import "./components/testimonials.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
 import ContactForm from "./components/contacts";
-=======
->>>>>>> 71afe43de23b6a525db37901833bc5150d96909b
->>>>>>> main
+
 
 const App = () => {
   return (
@@ -49,32 +35,7 @@ const App = () => {
         {/* <Footer /> */}
       </Router>
     </>
-<<<<<<< HEAD
-=======
-=======
-   <>
-   <Router >
-   {/* {<Navigation/>} */}
-   {/* {<LanguageSwitcher/>} */}
-   {/* {<Slideshow/>} */}
-   {/* {<Testimonial/>} */}
-    <Routes>
-          <Route path="/home" element={<Home/>} />
-          <Route path="/about" element={<About/>} />
-<<<<<<< HEAD
-          <Route path="/testzillow" element={<TestZillowApi />} />
-          {/* { <Route path="/products" element={<ProductList/>} /> } */}
-          {/* <Route path="/contact" element={<Contact />} /> */}
-=======
-          {/* <Route path="/products" element={<RentalsPage/>} /> 
-          <Route path="/contact" element={<ContactForm />} /> */}
->>>>>>> 71afe43de23b6a525db37901833bc5150d96909b
-    </Routes>
-  {/* <Footer /> */}
-  </Router>
-   </>
->>>>>>> main
->>>>>>> main
+
   );
 };
 
