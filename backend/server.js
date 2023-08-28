c5a7839cbd7143771e114123e487ac59f7dd9366
andresbenji <116680825+andresbenji@users.noreply.github.com>
2023-08-28 13:30:34 -0400

    server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  //set static folder
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
