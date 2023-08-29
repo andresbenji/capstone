02e63c550b2233d53dd29b8057d26ff898bb0651
andresbenji <116680825+andresbenji@users.noreply.github.com>
2023-08-29 00:38:02 -0400

    login/register

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,57 +1,57 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    phoneNumber: {
-      type: String,
-      required: true,
-      validate: {
-        validator: function (value) {
-          // Custom validation function
-          return value.length <= 9;
-        },
-        message: "Phone number must not exceed 9 characters.",
-      },
-    },
-    isAdmin: {
-      type: Boolean,
-      required: true,
-      default: false,
-    },
-  },
-  { timestamps: true }
-);
+// const userSchema = new mongoose.Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//     },
+//     email: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     password: {
+//       type: String,
+//       required: true,
+//     },
+//     phoneNumber: {
+//       type: String,
+//       required: true,
+//       validate: {
+//         validator: function (value) {
+//           // Custom validation function
+//           return value.length <= 9;
+//         },
+//         message: "Phone number must not exceed 9 characters.",
+//       },
+//     },
+//     isAdmin: {
+//       type: Boolean,
+//       required: true,
+//       default: false,
+//     },
+//   },
+//   { timestamps: true }
+// );
 
-//compares the entered password with the password taht is in the user database
-userSchema.methods.matchPassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
-  };
+// //compares the entered password with the password taht is in the user database
+// userSchema.methods.matchPassword = async function (enteredPassword) {
+//     return await bcrypt.compare(enteredPassword, this.password);
+//   };
   
-  //converts plain text password into hashed passowwrd before its saved into the database
-  userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-      next();
-    }
+//   //converts plain text password into hashed passowwrd before its saved into the database
+//   userSchema.pre('save', async function (next) {
+//     if (!this.isModified('password')) {
+//       next();
+//     }
   
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-  });
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+//   });
   
   
 
-const User = mongoose.model("User", userSchema);
+// const User = mongoose.model("User", userSchema);
 
-export default User;
+// export default User;
--- frontend/src/components/SignupPopup.jsx
+++ frontend/src/components/SignupPopup.jsx
@@ -1,95 +1,95 @@
-import React, { useState, useEffect } from 'react';
-import './SignUpForm.css';
+// import React, { useState, useEffect } from 'react';
+// import './SignUpForm.css';
 
-const SignupPopup = ({ isOpen, onClose }) => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  // const [ setEmail] = useState('');
-  // const [ setPassword] = useState();
-  const [isPopupOpen, setIsPopupOpen] = useState(isOpen);
+// const SignupPopup = ({ isOpen, onClose }) => {
+//   const [firstName, setFirstName] = useState('');
+//   const [lastName, setLastName] = useState('');
+//   // const [ setEmail] = useState('');
+//   // const [ setPassword] = useState();
+//   const [isPopupOpen, setIsPopupOpen] = useState(isOpen);
 
-  const handleFirstNameChange = (event) => {
-    setFirstName(event.target.value);
-  };
+//   const handleFirstNameChange = (event) => {
+//     setFirstName(event.target.value);
+//   };
 
-  const handleLastNameChange = (event) => {
-    setLastName(event.target.value);
-  };
+//   const handleLastNameChange = (event) => {
+//     setLastName(event.target.value);
+//   };
 
-  // const handleEmailChange = (event) => {
-  //   setPassword(event.target.value);
-  // };
+//   // const handleEmailChange = (event) => {
+//   //   setPassword(event.target.value);
+//   // };
 
-  // const handlePasswordChange = (event) => {
-  //   setPassword(event.target.value);
-  // };
+//   // const handlePasswordChange = (event) => {
+//   //   setPassword(event.target.value);
+//   // };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setFirstName('');
-    setLastName('');
-    // setEmail('');
-    // setPassword('');
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     setFirstName('');
+//     setLastName('');
+//     // setEmail('');
+//     // setPassword('');
 
-    onClose();
+//     onClose();
 
-  }
+//   }
 
-  useEffect(() => {
-    if (isOpen !== isPopupOpen){
-      setIsPopupOpen(isOpen)
-    }
-  }, [isOpen]);
-
-
-  if (!isOpen) return null;
+//   useEffect(() => {
+//     if (isOpen !== isPopupOpen){
+//       setIsPopupOpen(isOpen)
+//     }
+//   }, [isOpen]);
 
 
+//   if (!isOpen) return null;
 
-  return (
-    <div className={`signup-popup ${isOpen ? 'open' : ''}`}>
-    <div className="signup-popup">
-      <div className="popup-content">
-      <h2>Login or Sign Up</h2>
-        <form onSubmit={handleSubmit}>
 
-          <input
-          type="text"
-          placeholder="First Name"
-          value={firstName}
-          onChange={handleFirstNameChange}
-          required
-          />
 
-          <input
-          type="text"
-          placeholder="Last Name"
-          value={lastName}
-          onChange={handleLastNameChange}
-          required
-          />
+//   return (
+//     <div className={`signup-popup ${isOpen ? 'open' : ''}`}>
+//     <div className="signup-popup">
+//       <div className="popup-content">
+//       <h2>Login or Sign Up</h2>
+//         <form onSubmit={handleSubmit}>
 
-          <input
-            type="email"
-            placeholder="Email"
+//           <input
+//           type="text"
+//           placeholder="First Name"
+//           value={firstName}
+//           onChange={handleFirstNameChange}
+//           required
+//           />
+
+//           <input
+//           type="text"
+//           placeholder="Last Name"
+//           value={lastName}
+//           onChange={handleLastNameChange}
+//           required
+//           />
+
+//           <input
+//             type="email"
+//             placeholder="Email"
             
-            // onChange={handleEmailChange}
-            required
-          />
-          <input
-            type="password"
-            placeholder="Password"
+//             // onChange={handleEmailChange}
+//             required
+//           />
+//           <input
+//             type="password"
+//             placeholder="Password"
             
-            // onChange={handlePasswordChange}
-            required
-          />
-          <button onClick={onClose} type="submit">Submit</button>
+//             // onChange={handlePasswordChange}
+//             required
+//           />
+//           <button onClick={onClose} type="submit">Submit</button>
           
-        </form>
-        </div>
-      </div>
-    </div>
-  );
-};
+//         </form>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default SignupPopup
+// export default SignupPopup
